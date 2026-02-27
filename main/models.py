d51bb5f0cacfec56e3906f5d061bb5bc755147a4
zahar <zaharsilov70@gmail.com>
2026-02-27 18:54:05 +0300

    0

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    email = models.EmailField(unique=True, verbose_name="Email")
+    password = models.CharField(max_length=128, verbose_name="Пароль")  # Храните хеш, не открытый пароль!
+    role = models.BooleanField(default=False, verbose_name="Роль")  # False = обычный пользователь, True = админ
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
