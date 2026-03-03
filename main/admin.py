d3b802bd17c03943700fafbcaed93675a89af1ce
zahar <zaharsilov70@gmail.com>
2026-03-03 16:33:03 +0300

    хз

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,2 @@
 from django.contrib import admin
 
-from .models import Client, Librarian
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'last_name', 'email', 'role')
-
-@admin.register(Librarian)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'last_name', 'email', 'role')
