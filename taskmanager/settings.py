8db2d8c2f231fc89f64aca3f4bb45791615759ca
zahar <zaharsilov70@gmail.com>
2026-03-18 02:12:44 +0300

    smtp настройка

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -108,10 +108,10 @@ LOGIN_REDIRECT_URL = 'dashboard'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 PASSWORD_RESET_TIMEOUT = 86400
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False)
-EMAIL_USE_TLS = not EMAIL_USE_SSL
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 EMAIL_HOST_USER = 'abis.biblioteka@gmail.com'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'your-email@gmail.com'
